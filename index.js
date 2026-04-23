967d4d96776f1757cc20dfbc296cb268b0ba13cc
jeffrey <epstein>
2026-04-23 14:56:58 +0300

    new change2

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import { multiply } from "./multiply.js";
 const nodeInputOne = document.querySelector(".inputOne");
 const nodeInputTwo = document.querySelector(".inputTwo");
 const nodeBtn = document.querySelector(".btn");
-
+const result = document.querySelector('.result')
 
 
 nodeBtn.addEventListener("click", ()=>{
     console.log(multiply(nodeInputOne.value, nodeInputTwo.value));
+    result.innerHTML = `Результат: ${multiply(nodeInputOne.value, nodeInputTwo.value)}`;
+
 });
